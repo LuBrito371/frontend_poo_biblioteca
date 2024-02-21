7fa018a93f3f3f39bc67b2938e4e84522e74e592
Moonin371 <luanabrito285@gmail.com>
2024-02-20 23:15:33 -0300

    Listagem de Livros(Acervo)

--- src/main.js
+++ src/main.js
@@ -3,8 +3,21 @@ import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+// import axios from 'axios';
+
+// axios.defaults.baseURL = 'http://127.0.0.1:5173/';
+// axios.defaults.headers.common['Access-Control-Allow-Origin'] = 'http://localhost:8083';
+
+
 
 const app = createApp(App)
+
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+//     next();
+//   });
 
 app.use(router)
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import ViewLivros from '../views/ViewLivros.vue'
-import axios from 'axios';
 
-axios.defaults.baseURL = 'http://127.0.0.1:5173/';
-axios.defaults.headers.common['Access-Control-Allow-Origin'] = 'http://localhost:8083';
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
