7a300eed61d6e522b073ab9de24ce6da285b42ab
Moonin371 <luanabrito285@gmail.com>
2024-02-23 23:37:03 -0300

    Busca funcionando

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -18,6 +18,10 @@
                     <li class="nav-item">
                     <a class="nav-link" href="/addUsuario">Add Usuario</a>
                     </li>
+
+                    <li class="nav-item">
+                    <a class="nav-link" href="">Lista de Usuarios</a>
+                    </li>
                        
                 </ul>
                
